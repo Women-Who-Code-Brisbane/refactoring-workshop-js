b80e7a0997d582af8aa0b22d997d492586b5f3a4
Dominique Rolin <drolin@thoughtworks.com>
2017-01-24 00:06:18 +1000

    Removing temps

--- src/Customer.js
+++ src/Customer.js
@@ -12,21 +12,30 @@ Customer.prototype.addRental = function (rental) {
 Customer.prototype.getName = function () {
   return this._name;
 };
+Customer.prototype.getTotalCharge = function () {
+  return this._rentals.reduce(function (sum, each) {
+    sum += each.getCharge();
+    return sum;
+  }, 0);
+};
+
+Customer.prototype.getTotalFrequentRenterPoints = function () {
+  return this._rentals.reduce(function (sum, each) {
+    sum += each.getFrequentRenterPoints();
+    return sum;
+  }, 0);
+};
 
 Customer.prototype.statement = function () {
-  var totalAmount = 0;
-  var frequentRenterPoints = 0;
+
   var result = "Rental Record for " + this.getName() + "\n";
   for (var index in this._rentals) {
     var each = this._rentals[index];
-    frequentRenterPoints += each.getFrequentRenterPoints();
-    //show figures for this rental
     result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
-    totalAmount += each.getCharge();
   }
   //add footer lines result += "Amount
-  result += "Amount owed is " + totalAmount + "\n";
-  result += "You earned " + frequentRenterPoints + " frequent renter points";
+  result += "Amount owed is " + this.getTotalCharge() + "\n";
+  result += "You earned " + this.getTotalFrequentRenterPoints() + " frequent renter points";
   return result;
 };
 module.exports = Customer;
